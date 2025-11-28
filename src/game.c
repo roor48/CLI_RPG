972deb266ea194626826360a28b19fc6cea581e3
roor48 <initial_commit@naver.com>
2025-11-28 16:06:23 +0900

    cJson 추가
    body test

--- src/game.c
+++ src/game.c
@@ -56,8 +56,7 @@ unsigned long long startGame() {
 
 void initGame() {
 	Player player = (Player){ .health = 100, .level = 1 };
-	Inventory inventory = { 0 };
-	inventory.gold = 10000;
+	Inventory inventory = (Inventory){ .gold = 100 };
 
 	game = (Game){ .state = STATE_INIT, .player = player, .inventory = inventory };
 	initShop();
